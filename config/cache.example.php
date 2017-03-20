58d3194b3bc962ae13a590254cecc0b41fe335aa
Andrew DeChristopher <drew@kiir.us>
2017-03-20 10:26:40 -0400

    config/cache.example.php created.

---
+++ config/cache.example.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'cache' => [
+        'driver' => 'redis',
+        'connections' => [
+            'redis' => [
+                'host' => '127.0.0.1',
+                'port' => 6379,
+                'password' => null,
+            ]
+        ]
+    ]
+];
